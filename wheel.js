7cc47348f6f3aa7d30ce3f39b9a7ed208f73d408
Maciej J. Klimowicz <maciej.klimowicz@gmail.com>
2014-07-01 23:18:48 +0200

    Update wheel.js

--- wheel.js
+++ wheel.js
@@ -1,21 +1,20 @@
-  	$(function() {
-    	var $elie = $("#wheel"), degree = 0, timer;
-    	rotate();
-    	function rotate() {
-      	$elie.css({
-        	WebkitTransform: 'rotate(' + degree + 'deg)'});
-      	$elie.css({
-        	'-moz-transform': 'rotate(' + degree + 'deg)'});
-      	timer = setTimeout(function() {
-        	++degree;
-        	rotate();
-      	}
-                         	,30);
-    	}   	 
-    	$("input").toggle(function() {
-      	clearTimeout(timer);
-    	}
-                      	, function() {
-                        	rotate();
-                      	});
-  	});
+$(function() {
+	var $elie = $("#wheel"), degree = 0, timer;
+	rotate();
+	function rotate() {
+		$elie.css({
+			WebkitTransform: 'rotate(' + degree + 'deg)'});
+			$elie.css({
+				'-moz-transform': 'rotate(' + degree + 'deg)'});
+				timer = setTimeout(function() {
+					++degree;
+					rotate();
+					},30);
+					}
+					$("input").toggle(function() {
+						clearTimeout(timer);
+						}
+						, function() {
+							rotate();
+							});
+							});
