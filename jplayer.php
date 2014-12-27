c2daa6160e5c2204e66bf98c6702845b893c20ca
Maciej J. Klimowicz <maciej.klimowicz@gmail.com>
2014-12-27 11:54:33 +0100

    Create jplayer.php

---
+++ jplayer.php
@@ -0,0 +1,41 @@
+<div id="jquery_jplayer_1" class="jp-jplayer"></div>    
+    <div id="jp_container_1" class="jp-audio table">
+      <div class="jp-type-single container">
+        <div class="jp-gui jp-interface">
+          <ul class="jp-controls">            
+            <a href="javascript:;" tabindex="1" class="jp-play ani" title="Play" alt="Play">Play</a>
+            <a href="javascript:;" tabindex="1" class="jp-pause ani" title="Pause" alt="Pause">Pause</a>           
+            <a href="" tabindex="1" class="jp-playlist-toggle ani" title="Playlist" alt="Playlist">Playlist</a>
+          </ul>
+          <div class="jp-progress-bar">
+            <div class="jp-progress">
+              <div class="jp-seek-bar">
+                <div class="jp-play-bar"></div>
+              </div>
+            </div>
+            <div class="jp-time-holder">
+              <div class="jp-current-time"></div>
+              <div class="jp-duration"></div>
+            </div>
+            <div class="jp-title">
+              <ul>
+                <li></li>
+              </ul>
+            </div>
+          </div>
+          <div class="jp-volume-control">
+            <div class="jp-volume-bar">
+              <div class="jp-volume-bar-value"></div>            
+            </div>
+            <a href="javascript:;" tabindex="1" class="jp-mute ani" title="Mute" alt="Mute">Mute</a>
+            <a href="javascript:;" tabindex="1" class="jp-unmute ani" title="Unmute" alt="Unmute">Unmute</a>
+            <a href="javascript:;" tabindex="1" class="jp-volume-max ani" title="Max volume" alt="Max volume">Max volume</a>
+          </div>
+        </div>
+        <div class="jp-playlist">
+          <ul>
+            <!-- The method Playlist.displayPlaylist() uses this unordered list -->
+          </ul>
+        </div>
+      </div>
+    </div>
